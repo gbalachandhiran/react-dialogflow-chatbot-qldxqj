f6c68621c45789432d533f65a93163a112f89af4
gbalachandhiran <69426990+gbalachandhiran@users.noreply.github.com>
2021-11-28 10:29:33 +0530

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -142,6 +142,23 @@ const Message = ({ message, selectAge, selectGender, age, sex }) => {
     } else {
       return null;
     }
+  } else if (message.speech) {
+    return (
+      <div className="botCard">
+        <p
+          style={{
+            paddingLeft: '16px',
+            paddingRight: '10px',
+            fontFamily: 'Montserrat',
+            paddingTop: '7.5px',
+            paddingBottom: '7.5px',
+            fontWeight: 700,
+          }}
+        >
+          {message.text}
+        </p>
+      </div>
+    );
   } else {
     return (
       <div className="userCard">
